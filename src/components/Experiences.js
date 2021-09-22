4f36af4e5670dacbce64e2d52ddf9bb8fa85397e
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-23 01:31:38 +0530

    Update Skills.js

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Experiences extends React.Component {
-    render() { 
-        return <div></div>;
-    }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong>WORK-EXPERIENCES</strong>
+          </h6>
+          <div className="row">
+            <div className="col s12 m4 l4 xl4">
+                <p className="blue year_exp white-text">
+                    Feb
+                    <strong>2018</strong>
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Experiences;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // export default class Skills extends Component{
 //     render(){
 //         retutrn(
-            
+
 //         )
 //     }
 // }
 
 class Skills extends React.Component {
-    render() { 
-        return <div></div>;
-    }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong>PROFESSIONAL SKILLS</strong>
+          </h6>
+          <div className="row mt-top">
+            <div className="col s6">
+              <p>HTML</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "90%" }}></div>
+              </div>
+
+              <p>JAVASCRIPT</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "80%" }}></div>
+              </div>
+
+              <p>PHP</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "70%" }}></div>
+              </div>
+            </div>
+
+            <div className="col s6">
+              <p>CSS</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "99%" }}></div>
+              </div>
+
+              <p>JAVA</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "85%" }}></div>
+              </div>
+              <p>MYSQL</p>
+              <div class="progress gray lighten-1">
+                <div class="determinate blue" style={{ width: "60%" }}></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Skills;
