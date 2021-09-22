d300ed4a6275818a9442d5213be5cc4bfbbc08b1
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-22 13:19:10 +0530

    Add libraries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
