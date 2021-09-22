f226607c0f1047a13b5151d9a310701ca7a51e5f
ushanidesilva <ushanidesilva95@gmail.com>
2021-09-23 02:15:40 +0530

    Update Education.js and Experience.js

--- src/components/Educations.js
+++ src/components/Educations.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 class Educations extends React.Component {
-    render() { 
-        return <div></div>;
-    }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+            <h6><strong>EDUCATIONS</strong></h6>
+            <table className="striped">
+                <tr>
+                    <th>Qualification/ Certificate</th>
+                    <th>Date</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>Web Development</td>
+                    <td>Jan 2019</td>
+                    <td><Link to="#" className="btn blue lighten-2">View</Link></td>
+                </tr>
+                <tr>
+                    <td>Web Development</td>
+                    <td>Jan 2019</td>
+                    <td><Link to="#" className="btn blue lighten-2">View</Link></td>
+                </tr>
+                <tr>
+                    <td>Web Development</td>
+                    <td>Jan 2019</td>
+                    <td><Link to="#" className="btn blue lighten-2">View</Link></td>
+                </tr>
+            </table>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Educations;
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -3,18 +3,66 @@ import React, { Component } from "react";
 class Experiences extends React.Component {
   render() {
     return (
-      <div className="card">
-        <div className="card-content">
-          <h6>
-            <strong>WORK-EXPERIENCES</strong>
-          </h6>
-          <div className="row">
-            <div className="col s12 m4 l4 xl4">
-                <p className="blue year_exp white-text">
-                    Feb
-                    <strong>2018</strong>
+      <div>
+        <div className="card">
+          <div className="card-content">
+            <h6>
+              <strong>WORK-EXPERIENCES</strong>
+            </h6>
+            <div className="row">
+              <div className="col s12 m4 l4 xl4">
+                <p className="teal year_exp white-text">
+                  Feb&nbsp;
+                  <strong>2016</strong>&nbsp;-&nbsp;Jan&nbsp;
+                  <strong>2018</strong>
                 </p>
+              </div>
+              <div className="col s12 m8 l8 xl8">
+                <blockquote className="no-pad">
+                  <h6 className="no-pad mt-bottom">
+                    <strong>Web Developer - Intern</strong>
+                  </h6>
+                  <p className="gray-text">
+                    Diligent software engineer with 5+ years experience in
+                    commercial application development. Eager to join Cyclone
+                    Inc. to build innovative and cutting edge business solutions
+                    for the impressive suite of clients within its global reach.
+                    In previous roles, slashed downtime by 25% and ensured 98%
+                    on-time project completion. Also identified and dealt with a
+                    significant process bottleneck that boosted coding
+                    efficiency by 35% when resolved.
+                  </p>
+                </blockquote>
+              </div>
             </div>
+
+            <div className="row">
+              <div className="col s12 m4 l4 xl4">
+                <p className="teal year_exp white-text">
+                  Feb&nbsp;
+                  <strong>2016</strong>&nbsp;-&nbsp;Jan&nbsp;
+                  <strong>2018</strong>
+                </p>
+              </div>
+              <div className="col s12 m8 l8 xl8">
+                <blockquote className="no-pad">
+                  <h6 className="no-pad mt-bottom">
+                    <strong>Web Developer - Intern</strong>
+                  </h6>
+                  <p className="gray-text">
+                    Diligent software engineer with 5+ years experience in
+                    commercial application development. Eager to join Cyclone
+                    Inc. to build innovative and cutting edge business solutions
+                    for the impressive suite of clients within its global reach.
+                    In previous roles, slashed downtime by 25% and ensured 98%
+                    on-time project completion. Also identified and dealt with a
+                    significant process bottleneck that boosted coding
+                    efficiency by 35% when resolved.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
